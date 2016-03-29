87ba8b19cbc69a547ebac695d96eea07fd9e60c3
JamesDuBois1994 <jamesdubois12@gmail.com>
2016-03-29 14:50:40 -0400

    Login POST WORKING

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,37 +1,40 @@
 <?php
  class Users extends CI_Controller {
 
- 	public function show($user_id) {
- 		// Use user model manually
- 		//$this->load->model('user_model');
+ // 	public function show($user_id) {
+ // 		// Use user model manually
+ // 		//$this->load->model('user_model');
 
- 		// get model function -> results is going to have an array of objects inside of it.
- 		$data['results'] =  $this->user_model->get_users($user_id, 'test1');
+ // 		// get model function -> results is going to have an array of objects inside of it.
+ // 		$data['results'] =  $this->user_model->get_users($user_id, 'test1');
 
 
 
- 			// load that view
-		$this->load->view('user_view', $data);
+ // 			// load that view
+	// 	$this->load->view('user_view', $data);
 
+ // 	}
+
+ // 		public function update(){
+	// 	// Insert some information
+ // 			$id = 3;
+ // 			$username = 'Robert';
+ // 			$password = 'updatedPasswrod';
+
+	// 	$this->user_model->update_users([
+	// 		'username' => $username,
+	// 		'password' => $password
+	// 		], $id);
+	// }
+
+	// 	public function delete() {
+	// 	$id = 3;
+	// 	$this->user_model->delete_users($id);
+	// 	}
+
+ 	public function login() {
+ 		echo $_POST['username'];
  	}
-
- 		public function update(){
-		// Insert some information
- 			$id = 3;
- 			$username = 'Robert';
- 			$password = 'updatedPasswrod';
-
-		$this->user_model->update_users([
-			'username' => $username,
-			'password' => $password
-			], $id);
-	}
-
-		public function delete() {
-		$id = 3;
-		$this->user_model->delete_users($id);
-		}
-
 
 
  }
--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -3,14 +3,61 @@
 <?php // Open the form and close it on the login view page ?>
 
 <?php $attributes = array('id' => 'login_form', 'class' => 'form_horizontal') ?>
-<?php echo form_open('users/login_view', $attributes); ?>
+<?php echo form_open('users/login', $attributes); ?>
 
 
 <div class="form-group">
 	<?php echo form_label("Username"); ?>
 
-	<?php form_input(); ?>
+	<?php  
+		$data = array(
+			'class' => 'form-control',
+			'name' => 'username',
+			'placeholder' => 'Enter Username',
+			);
 	
+	// Apply values to form
+	 echo form_input($data); 
+
+
+	 ?>
+
+</div>
+
+<div class="form-group">
+	<?php echo form_label("Password"); ?>
+
+	<?php  
+		$data = array(
+			'class' => 'form-control',
+			'name' => 'password',
+			'placeholder' => 'Enter Password',
+			);
+	
+	// Apply values to form
+	 echo form_password($data); 
+
+
+	 ?>
+
+</div>
+
+<div class="form-group">
+
+
+	<?php  
+		$data = array(
+			'class' => 'btn btn-primary',
+			'name' => 'submit',
+			'value' => 'Login',
+			);
+	
+	// Apply values to form
+	 echo form_submit($data); 
+
+
+	 ?>
+
 </div>
 
 
