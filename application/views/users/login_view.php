e1f30e480720dd197a2ead3bd8202b81acb3b393
JamesDuBois1994 <jamesdubois12@gmail.com>
2016-03-29 14:20:53 -0400

    Working on Login form

--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,1 +1,42 @@
 <h2>Login Form</h2>
+
+<?php // Open the form and close it on the login view page ?>
+
+<?php $attributes = array('id' => 'login_form', 'class' => 'form_horizontal') ?>
+<?php echo form_open('users/login_view', $attributes); ?>
+
+
+<div class="form-group">
+	<?php echo form_label("Username"); ?>
+
+	<?php form_input(); ?>
+	
+</div>
+
+
+
+
+
+
+
+
+
+
+<?php echo form_close(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
